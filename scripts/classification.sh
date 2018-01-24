8c2c356fc69677d9824c55bd2a9efe978234240f
szxwpp <1039194607@qq.com>
2018-01-24 11:30:45 +0800

    init

---
+++ scripts/classification.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+ROOT_DIR="$( cd "$(dirname "$0")" ; cd .. ; pwd -P )"
+echo $ROOT_DIR
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$ROOT_DIR/lib/caffe
+echo $LD_LIBRARY_PATH
+
+sudo chmod a+x $ROOT_DIR/lib/caffe/create_link.sh
+bash $ROOT_DIR/lib/caffe/create_link.sh
+
+
+sudo chmod a+x $ROOT_DIR/bin/classification
+
+$ROOT_DIR/bin/classification \
+$ROOT_DIR/resource/caffenet/deploy.prototxt \
+$ROOT_DIR/resource/caffenet/bvlc_reference_caffenet.caffemodel \
+$ROOT_DIR/resource/caffenet/imagenet_mean.binaryproto \
+$ROOT_DIR/resource/caffenet/synset_words.txt \
+$ROOT_DIR/resource/caffenet/cat.jpg
